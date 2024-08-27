49ae6080c0d1b2484b804ccd5b0318aee434a13f
vina <vina@VIAWSE02LC0429.duoc.acad>
2024-08-26 21:58:50 -0400

    Condiciones

---
+++ src/ejercicio3/Condiciones.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author Diego "cuarta clase" Cabrera
+ */
+public class Condiciones {
+    
+    private int edad;
+    private String nombre;
+    
+    //Sin parametros por que no tiene nada
+    public Condiciones() {
+    }
+
+    //Con parametros por que tiene cosas dentro del parentesis
+    public Condiciones(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
